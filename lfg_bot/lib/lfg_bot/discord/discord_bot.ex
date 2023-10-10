1fa67882a7779093849e803e16a34aefae2e06f0
Geordie Powers <gp@gpow.ca>
2023-10-09 23:38:07 -0400

    send and edit session message

--- lfg_bot/lib/lfg_bot/discord/discord_bot.ex
+++ lfg_bot/lib/lfg_bot/discord/discord_bot.ex
@@ -1,42 +1,50 @@
 defmodule LfgBot.Discord.Bot do
   use Nostrum.Consumer
+
+  alias LfgBot.LfgSystem
+  alias LfgBot.LfgSystem.Session
   alias Nostrum.Api
+  alias Nostrum.Struct.Interaction
+  alias Nostrum.Struct.ApplicationCommandInteractionData
+  alias Nostrum.Struct.User
+  alias Nostrum.Snowflake
+  alias Nostrum.Struct.Message
+  alias Nostrum.Struct.Component.ActionRow
+  alias Nostrum.Struct.Component.Button
+  alias Nostrum.Struct.Embed
 
   # bot permissions = send messages, create public threads, manage threads, read message history, add reactions, use slash commands
   # @bot_invite_url "https://discord.com/api/oauth2/authorize?client_id=1160972219061645312&permissions=53687158848&scope=bot"
   # bot user id aka application id
   @bot_user_id 1_160_972_219_061_645_312
 
-  # ignore messages from the bot
-  def handle_event({:MESSAGE_CREATE, msg, _ws_state}) when msg.author.id == @bot_user_id,
-    do: :noop
+  @command_name "lfginit"
 
   def handle_event({:READY, %{guilds: guilds}, _ws_state}) do
     command = %{
-      name: "lfginit",
-      description:
-        "Tell LFG Bot to register or un-register the current channel. Registering only needs to be done once.",
-      options: [
-        %{
-          # reference for type numbers: https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types
-          # more reference for type numbers: https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
-          type: 3,
-          name: "register",
-          description:
-            "whether or not the bot should use this channel to manage LFG groups and games",
-          required: true,
-          choices: [
-            %{
-              name: "Register",
-              value: "unregister"
-            },
-            %{
-              name: "Un-Register",
-              value: "unregister"
-            }
-          ]
-        }
-      ]
+      name: @command_name,
+      description: "Initialize LFG Bot in the current channel"
+      # options:
+      #   %{
+      #     # reference for type numbers: https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types
+      #     # more reference for type numbers: https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
+      #     type: 3,
+      #     name: "register",
+      #     description:
+      #       "whether or not the bot should use this channel to manage LFG groups and games",
+      #     required: true,
+      #     choices: [
+      #       %{
+      #         name: "Register",
+      #         value: "unregister"
+      #       },
+      #       %{
+      #         name: "Un-Register",
+      #         value: "unregister"
+      #       }
+      #     ]
+      #   }
+      # ]
     }
 
     # on startup, delete all existing commands we've previously created, and re-register
@@ -52,62 +60,179 @@ defmodule LfgBot.Discord.Bot do
     end)
   end
 
-  # TODO: an interaction create handler that matches on LFGBOT_START_GAME
   def handle_event(
         {:INTERACTION_CREATE,
-         %Nostrum.Struct.Interaction{channel_id: channel_id, data: data} = interaction, _}
+         %Interaction{
+           data: %ApplicationCommandInteractionData{
+             custom_id: "LFGBOT_SHUFFLE_TEAMS_" <> session_id
+           }
+         }, _ws_state}
       ) do
-    import Bitwise
+  end
 
-    IO.puts("INTERACTION_CREATE in channel #{channel_id}")
-    IO.inspect(interaction)
+  def handle_event(
+        {:INTERACTION_CREATE,
+         %Interaction{
+           user: %{
+             id: user_id
+           },
+           data: %ApplicationCommandInteractionData{
+             custom_id: "LFGBOT_END_SESSION_" <> session_id
+           }
+         }, _ws_state}
+      ) do
+    # with {:ok, session} <- LfgSystem.get(Session, session_id),
+    # {:ok, %{state: :ended}} <-  do
+    # end
+  end
 
-    alias Nostrum.Struct.Component.ActionRow
-    alias Nostrum.Struct.Component.Button
+  def handle_event(
+        {:INTERACTION_CREATE,
+         %Interaction{
+           data: %ApplicationCommandInteractionData{
+             custom_id: "LFGBOT_PLAYER_JOIN_" <> session_id
+           }
+         }, _ws_state}
+      ) do
+  end
+
+  def handle_event(
+        {:INTERACTION_CREATE,
+         %Interaction{
+           data: %ApplicationCommandInteractionData{
+             custom_id: "LFGBOT_PLAYER_LEAVE_" <> session_id
+           }
+         }, _ws_state}
+      ) do
+  end
+
+  def handle_event(
+        {:INTERACTION_CREATE,
+         %Interaction{
+           channel_id: channel_id,
+           guild_id: guild_id,
+           data: %ApplicationCommandInteractionData{custom_id: "LFGBOT_START_SESSION"}
+         }, _ws_state}
+      ) do
+    {:ok, %{id: setup_msg_id, author: author} = message} =
+      Api.create_message(channel_id, content: "Setting up a new game...")
+
+    with %{id: leader_user_id, username: leader_user_name} <- author,
+         {:ok, session} <-
+           Session.new(%{
+             guild_id: Snowflake.dump(guild_id),
+             channel_id: Snowflake.dump(channel_id),
+             message_id: Snowflake.dump(setup_msg_id),
+             leader_user_id: Snowflake.dump(leader_user_id),
+             leader_user_name: leader_user_name
+           }) do
+      {:ok, _message} =
+        Api.edit_message(channel_id, setup_msg_id,
+          content: LfgBot.LfgSystem.Session.Utils.build_sesson_message(session),
+          components: build_session_buttons(session.id)
+        )
+    else
+      anything ->
+        IO.inspect(anything)
+        Api.delete_message(channel_id, setup_msg_id)
+    end
+  end
+
+  def handle_event(
+        {:INTERACTION_CREATE,
+         %Interaction{data: %ApplicationCommandInteractionData{name: @command_name}} =
+           interaction, _}
+      ) do
+    description = """
+    A Discord bot for inhouse games
+    *by <@84203400920563712>*
+
+    **Click 'New Game' below to start a group!**
+    The bot will send a message in this channel with buttons to join/leave the group, shuffle teams, and end the session.
+
+    Buttons with the 🔒 emoji can only be used by the group creator.
+
+    Have fun!
+    """
+
+    message_embed =
+      %Embed{}
+      |> Embed.put_title("LFG Bot")
+      # fields for later, once repo is public:
+      # |> Embed.put_author("GP")
+      # |> Embed.put_url("https://github.com/geordiep/lfg_bot")
+      |> Embed.put_color(0xFF6600)
+      |> Embed.put_description(description)
 
     # docs:
     # button options - presumably default discord options: https://discord.com/developers/docs/interactions/message-components#button-object-button-structure
     action_row =
       ActionRow.action_row()
       |> ActionRow.append(
-        Button.interaction_button("Start game", "LFGBOT_START_GAME",
-          style: Nostrum.Constants.ButtonStyle.primary(),
-          emoji: %{name: "🔥"}
+        Button.interaction_button("New Game", "LFGBOT_START_SESSION",
+          style: Nostrum.Constants.ButtonStyle.success()
         )
       )
 
+    import Bitwise
     # response docs:
     # type: https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-response-object-interaction-callback-type
     # data: https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-response-object-interaction-callback-data-structure
     # data.flags: https://discord.com/developers/docs/resources/channel#message-object-message-flags
     response = %{
-      # ChannelMessageWithSource
       type: 4,
       data: %{
-        content: "Channel registered",
-        flags: 1 <<< 6,
-        components: [action_row]
+        # suppress notifications: flag 1<<12
+        flags: 1 <<< 12,
+        components: [action_row],
+        embeds: [
+          message_embed
+        ]
       }
     }
 
-    IO.puts("sending response:")
-    IO.inspect(response)
     Api.create_interaction_response(interaction, response)
   end
 
-  def handle_event({:MESSAGE_CREATE, msg, ws_state}) do
-    unless msg.author.bot do
-      IO.puts("got message event. message is:")
-      IO.puts("---")
-      IO.inspect(msg)
-      IO.puts("---")
-    else
-      IO.puts("got a bot message event")
-      IO.puts("---")
-      IO.inspect(msg)
-      IO.puts("---")
-    end
-  end
+  # # ignore messages from bots
+  # def handle_event({:MESSAGE_CREATE, msg, _ws_state}) when msg.author.bot,
+  #   do: :noop
 
   def handle_event(_), do: :noop
+
+  # ------------------
+
+  defp build_session_buttons(session_id) do
+    leader_buttons_row =
+      ActionRow.action_row()
+      |> ActionRow.append(
+        Button.interaction_button("Shuffle Teams", "LFGBOT_SHUFFLE_TEAMS_" <> session_id,
+          style: Nostrum.Constants.ButtonStyle.primary(),
+          emoji: %{name: "🔒"}
+        )
+      )
+      |> ActionRow.append(
+        Button.interaction_button("End Session", "LFGBOT_END_SESSION_" <> session_id,
+          style: Nostrum.Constants.ButtonStyle.secondary(),
+          emoji: %{name: "🔒"}
+        )
+      )
+
+    user_buttons_row =
+      ActionRow.action_row()
+      |> ActionRow.append(
+        Button.interaction_button("Join Game", "LFGBOT_PLAYER_JOIN_" <> session_id,
+          style: Nostrum.Constants.ButtonStyle.success(),
+          emoji: %{name: "🎮"}
+        )
+      )
+      |> ActionRow.append(
+        Button.interaction_button("Leave Game", "LFGBOT_PLAYER_LEAVE_" <> session_id,
+          style: Nostrum.Constants.ButtonStyle.secondary(),
+          emoji: %{name: "🚶"}
+        )
+      )
+
+    [leader_buttons_row, user_buttons_row]
+  end
 end
